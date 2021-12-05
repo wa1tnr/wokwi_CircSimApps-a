1a9fa6549f5b781999deca6373b7d55e4c2e19f1
wa1tnr <wa1tnr@users.noreply.github.com>
2021-12-05 12:56:05 +0000

    skeletals - previously developed code

--- series-aa.d/blink_9_Hz-a/blink_9_Hz-a.ino
+++ series-aa.d/blink_9_Hz-a/blink_9_Hz-a.ino
@@ -1,3 +1,37 @@
-// Sun  5 Dec 12:23:20 UTC 2021
-void setup(void) { } void loop(void) { }
+// Sun  5 Dec 12:54:47 UTC 2021
+
+#define i9_Hz 55 // milliseconds
+
+void pulse_once(int interval) { }
+
+void pulse_at_9_Hz(void) {
+        pulse_once(i9_Hz);
+}
+
+void pulse(void) { // alias
+    pulse_at_9_Hz();
+}
+
+void setup_gpio(void) { }
+
+void setup_serial(void) { }
+
+void bring_stuff_up(void) {
+    setup_gpio();
+    setup_serial();
+}
+
+void blinking(void) {
+    pulse();
+}
+
+void setup(void) {
+    bring_stuff_up();
+    blinking();
+}
+
+void loop(void) {
+    while(-1); // endless
+}
+
 // END.
