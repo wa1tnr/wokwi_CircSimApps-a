82938011fede7e3c8d60f3966baaa5b4db8ae551
wa1tnr <wa1tnr@users.noreply.github.com>
2021-12-28 05:06:47 +0000

    fairly complete reporting

--- series-cc.d/printf_a/write_b.c
+++ series-cc.d/printf_a/write_b.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <string.h> // strlen only
 
 void space_it(void) {
     printf("%c", ' ');
@@ -15,7 +16,8 @@ void newline(void) {
 }
 
 int main(void) {
-    char buffer[20];
+    newline();
+    char buffer[32];
     char* buf_ptr;
 
     /* ssize_t write(int fd, const void *buf, size_t count); */
@@ -27,35 +29,43 @@ int main(void) {
 
     buf_ptr = buffer;
 
-    int buf_size; buf_size = sizeof(buffer); // captures "abc\000" size
+
+    int buf_size, buf_ptr_size;
+
+    buf_size = sizeof(buffer); // captures "abc\000" size
+    buf_ptr_size = sizeof(buf_ptr);
+
+    int buf_len = strlen(buffer);
 
     ssize_t gottem;
 
-    newline();
+    printf("%s ", "is the buffer contents");
+
     gottem = write(1, buf_ptr, sizeof(buf_ptr));
 
-    // format ssize_t gottem for printing
-    // answers the question about the size of .. something.  What? (it prints an '8')
-    sprintf(buf_ptr, "\n\n%d", gottem);
-
-    // print to the terminal the formatted data (gottem) now held in the buffer
+    sprintf(buf_ptr, "\n           sizeof(buf_ptr) is  %d", buf_ptr_size);
     printf(buf_ptr);
 
-    sprintf(buf_ptr, "\n\n%d", buf_size); // related to string length, possibly
+    printf("%s", "\n           sizeof(buffer)  is ");
+    sprintf(buf_ptr, "%d\n", buf_size); // related to string length, possibly
     printf(buf_ptr);
 
+    printf("%s", "           strlen(buffer)  is ");
+// ##bookmark
+    sprintf(buf_ptr, " %d\n", buf_len); // related to string length, possibly
+    printf(buf_ptr);
 
     uint64_t adrs;
     adrs = (uint64_t) & buf_ptr;
 
+    printf("%s", "adrs (& buf_ptr) in hex is      ");
     // print the buffer's address in ram
-    sprintf(buf_ptr, "\n     %.8X", adrs);
-    space_it();
-
+    sprintf(buf_ptr, "0x%.8X", adrs);
     printf(buf_ptr);
-    sprintf(buf_ptr, "\n  %.11u\n\n", adrs);
 
-    space_it();
+    printf("%s", "\nadrs (& buf_ptr) in decimal is ");
+    sprintf(buf_ptr, "%.11u\n\n", adrs);
+
     printf(buf_ptr);
 }
 
