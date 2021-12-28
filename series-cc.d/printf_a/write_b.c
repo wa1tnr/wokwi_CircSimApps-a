93a7309a1da44d1d22a8e02b23a43f4342e076c4
wa1tnr <wa1tnr@users.noreply.github.com>
2021-12-28 04:05:48 +0000

    now prints buffer size also

--- series-cc.d/printf_a/write_b.c
+++ series-cc.d/printf_a/write_b.c
@@ -16,7 +16,7 @@ void newline(void) {
 
 int main(void) {
     char buffer[20];
-    char* buf_ptr, buf_buf_ptr;
+    char* buf_ptr;
 
     /* ssize_t write(int fd, const void *buf, size_t count); */
 
@@ -27,24 +27,32 @@ int main(void) {
 
     buf_ptr = buffer;
 
-    ssize_t gottem;
+    int buf_size; buf_size = sizeof(buffer); // captures "abc\000" size
 
+    ssize_t gottem;
 
     newline();
     gottem = write(1, buf_ptr, sizeof(buf_ptr));
 
+    // format ssize_t gottem for printing
+    // answers the question about the size of .. something.  What? (it prints an '8')
     sprintf(buf_ptr, "\n\n%d", gottem);
+
+    // print to the terminal the formatted data (gottem) now held in the buffer
     printf(buf_ptr);
+
+    sprintf(buf_ptr, "\n\n%d", buf_size); // related to string length, possibly
+    printf(buf_ptr);
+
 
     uint64_t adrs;
     adrs = (uint64_t) & buf_ptr;
 
-    // sprintf(buf_ptr, "\n  %.12X", adrs);
+    // print the buffer's address in ram
     sprintf(buf_ptr, "\n     %.8X", adrs);
     space_it();
 
     printf(buf_ptr);
-    // sprintf(buf_ptr, "\n  %.16u\n\n", adrs);
     sprintf(buf_ptr, "\n  %.11u\n\n", adrs);
 
     space_it();
