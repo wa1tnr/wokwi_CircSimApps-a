d4df475839533cc9a2f16133b453e3b0b2469be8
wa1tnr <wa1tnr@users.noreply.github.com>
2022-03-10 12:42:13 +0000

    remove commented code

--- series-ff.d/morse-a/n_sketch_morse.cpp
+++ series-ff.d/morse-a/n_sketch_morse.cpp
@@ -7,41 +7,12 @@
 #include <FastLED.h>
 #include "sketch_morse.h"
 
-// #define LED_PIN  5
-// #define NUM_LEDS 24 // 240
 CRGB leds[NUM_LEDS];
 
-// #define BACKGROUND_COLOR CRGB::Blue
-// #define FOREGROUND_COLOR CRGB::Red
 
 const int potPin = A0;
 
 byte buffer[40];
-
-// #define MORSE_DOT '.'
-// #define MORSE_DASH '-'
-// #define MORSE_INTERCODE ' '
-// #define MORSE_INTERCHAR '#'
-
-// #define MORSE_DOT_LENGTH 1
-// #define MORSE_DASH_LENGTH 3
-// #define MORSE_INTERCODE_LENGTH 3
-// #define MORSE_INTERCHAR_LENGTH 8
-
-#if 0
-const byte morseTable[] PROGMEM =
-  "#  " // a space; a gap between words
-  "#A.-" "#B-..." "#C-.-." "#D-.." "#E." "#F..-."
-  "#G--." "#H...." "#I.." "#J.---" "#K-.-" "#L.-.."
-  "#M--" "#N-." "#O---" "#P.--." "#Q--.-" "#R.-."
-  "#S..." "#T-" "#U..-" "#V...-" "#W.--" "#X-..-"
-  "#Y-.--" "#Z--.." "#1.----" "#2..---" "#3...--"
-  "#4....-" "#5....." "#6-...." "#7--..." "#8---.."
-  "#9----." "#0-----" "#..-.-.-" "#;-.-.-."
-  "#\--..--" "#?..--.." "#/-..-."
-  "#\r  " "#\n  "
-  "#";
-#endif
 
 void setup(void) {
   Serial.begin(115200);
