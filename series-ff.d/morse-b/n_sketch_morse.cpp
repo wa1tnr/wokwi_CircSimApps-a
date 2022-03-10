6e8c28040f874e7f517900cebb585c31cf5d9357
wa1tnr <wa1tnr@users.noreply.github.com>
2022-03-10 20:54:22 +0000

    edited

--- series-ff.d/morse-b/n_sketch_morse.cpp
+++ series-ff.d/morse-b/n_sketch_morse.cpp
@@ -1,7 +1,7 @@
-// Thu 10 Mar 20:09:16 UTC 2022 cd jklm OFFLINE datestamped edit
+// Thu 10 Mar 20:51:55 UTC 2022 cd klmn ONLINE datestamped edit
 // bit of a mess now. ;)
 
-#define ID_IN_SERIAL_MON(x) Serial.println("yj724b-gg")
+#define ID_IN_SERIAL_MON(x) Serial.println("yj724b-hh")
 
 // 13:17z - factored 'every_n_ms()'
 
@@ -23,176 +23,137 @@ byte buffer[80];
 
 // very first factored function to clear the display:
 void cls(void) {
-    for( int i=0; i<NUM_LEDS+0; i++) {
-        leds[i] = CRGB::AntiqueWhite; // BACKGROUND_COLOR;
-    }
-    FastLED.show();
+  for ( int i = 0; i < NUM_LEDS + 0; i++) {
+    leds[i] = CRGB::AntiqueWhite; // BACKGROUND_COLOR;
+  }
+  FastLED.show();
 }
 
 unsigned long counter = 0;
 
 // second factored function to do something every n milliseconds
 void every_n_ms(bool calling) {
-    EVERY_N_MILLISECONDS( 200) { // from the FastLED library
-        counter++;
-        // TRAP:
-        if (counter > 999999) { while(-1); }
-#if 0
-        Serial.write(' ');
-        Serial.print(counter);
-        Serial.write(' ');
-#endif
-
-        // void bump_leds(void) {
-        for( int i=NUM_LEDS-1; i>0; i--) {
-            leds[i] = leds[i-1];
-        }
-        // }
-
-        leds[0] = LEAD_OR_TRAIL; // BACKGROUND_COLOR;
-
-        bool not_trunc = false;
-        bool nospace = false;
-        int n = strlen(buffer);
-
-
-        if (n > 0) { not_trunc = true; } // not_truncated
-        if (buffer[0] != ' ') { nospace = true; }
-
-#if 0
-        Serial.print("\nstrlen buffer is: ");
-
-        Serial.print(n); Serial.write(' '); Serial.write('Q');
-
-        if (not_trunc && nospace) { Serial.print(" 'not_trunc and nospace' "); }
-
-
-        if (not_trunc) { Serial.print(" not_trunc "); }
-        if (nospace)   { Serial.print(" nospace ");   }
-#endif
-
-        if (not_trunc & nospace) { leds[0] = FOREGROUND_COLOR; }
-        if (not_trunc & !nospace) { leds[0] = CRGB::Turquoise; }
-
-        for( int i=0; i<n; i++) {
-            buffer[i] = buffer[i+1];
-#if 0
-            Serial.write('.'); }
-#endif
-        FastLED.show(); // make the leds[] data visible
+  EVERY_N_MILLISECONDS( 200) { // from the FastLED library
+    counter++;
+    // TRAP:
+    if (counter > 999999) {
+      while (-1);
     }
+    // void bump_leds(void) {
+    for ( int i = NUM_LEDS - 1; i > 0; i--) {
+      leds[i] = leds[i - 1];
+    }
+    // }
+
+    leds[0] = LEAD_OR_TRAIL; // BACKGROUND_COLOR;
+
+    bool not_trunc = false;
+    bool nospace = false;
+    int n = strlen(buffer);
+
+    if (n > 0) {
+      not_trunc = true;  // not_truncated
+    }
+    if (buffer[0] != ' ') {
+      nospace = true;
+    }
+    if (not_trunc & nospace) {
+      leds[0] = FOREGROUND_COLOR;
+    }
+    if (not_trunc & !nospace) {
+      leds[0] = CRGB::Turquoise;
+    }
+
+    for ( int i = 0; i < n; i++) {
+      buffer[i] = buffer[i + 1];
+    }
+    FastLED.show(); // make the leds[] data visible
+  }
 }
 
 void interchar_sp(void) {
-    strcat( buffer, "     "); // spacing between characters
+  strcat( buffer, "     "); // spacing between characters
 }
 
 void setup(void) {
-    Serial.begin(115200); Serial.write(' ');
-    buffer[0] = '\0';
-    ID_IN_SERIAL_MON();
-    FastLED.addLeds<WS2811, LED_PIN, GRB>(leds, NUM_LEDS);
-    cls();
+  Serial.begin(115200); Serial.write(' ');
+  buffer[0] = '\0';
+  ID_IN_SERIAL_MON();
+  FastLED.addLeds<WS2811, LED_PIN, GRB>(leds, NUM_LEDS);
+  cls();
 }
 
 void loop(void) {
-    bool call = false;
-    every_n_ms(call);
+  bool call = false;
+  every_n_ms(call);
 
-if( strlen( buffer) == 0) // ready to accept new data ?
-{
+  if (strlen( buffer) == 0) { // ready to accept new data ?
+    int available_chars = Serial.available();
+    if ( available_chars > 0) {
+      byte serialChar = Serial.read();
+      serialChar = toupper( serialChar); // no lower case in the morse code
+      if (available_chars == 1) {
+        call = true;
+      }
+      if (call) {
+        interchar_sp();
+      }
 
-int available_chars = Serial.available();
+      // search for character in the table
+      bool marker = false; // a test for '#' marker
+      bool found = false;
+      int index;
+      byte morseChar;
 
-if( available_chars > 0)
-{
+      for ( index = 0; index < strlen_P(morseTable); index++) {
+        byte morseChar = pgm_read_byte_near( morseTable + index);
 
-byte serialChar = Serial.read();
-serialChar = toupper( serialChar); // no lower case in the morse code
+        if ( marker) { // was previous character the marker ?
+          if ( serialChar == morseChar) { // found the ASCII character ?
+            found = true;
+            break;
+          }
+        }
 
-if (available_chars == 1) { call = true; }
+        if ( morseChar = '#') // found marker ?
+          marker = true;
+        else
+          marker = false;
+      }
 
-if (call) {
-    interchar_sp();
-#if 0
-    Serial.write('c');
-#endif
-}// KLUDGE/EXP 18:22z
-
-// search for character in the table
-bool marker = false; // a test for '#' marker
-bool found = false;
-int index;
-byte morseChar;
-
-for( index=0; index<strlen_P(morseTable); index++)
-{ 
-byte morseChar = pgm_read_byte_near( morseTable + index);
-
-if( marker) // was previous character the marker ?
-{
-if( serialChar == morseChar) // found the ASCII character ?
-{
-found = true;
-break;
-}
-}
-
-if( morseChar = '#') // found marker ?
-marker = true;
-else
-marker = false;
-}
-
-if( found)
-{
-// Convert the character into morse code and put it in the buffer
-// This is the hard part
-// For now, just use a fixed pattern
-index++; // point to data after the ASCII character
-do
-{
-morseChar = pgm_read_byte_near( morseTable + index);
-
-if( morseChar == '#') // marker for new character ?
-{
-break;
-}
-else
-{
-if( morseChar == '.')
-strcat( buffer, "X ");
-else if( morseChar == '-')
-strcat( buffer, "XXX ");
-else if( morseChar == ' ')
-strcat( buffer, " ");
-}
-
-index++;
-
-} while( morseChar != '\0');
-interchar_sp();
-// strcat( buffer, "     "); // spacing between characters
-}
-else
-{
-Serial.print("Character not found in morse table: ");
-// The isPrintable() is a Arduino function
-// The isprint() is a 'C' function
-if( isprint( serialChar)) 
-{
-Serial.print( (char) serialChar);
-}
-else
-{
-Serial.print( "0x");
-if( serialChar < 0x10)
-Serial.print( "0");
-Serial.print( serialChar, HEX);
-}
-Serial.println();
-}
-} 
-}
+      if ( found) {
+        // Convert the character into morse code and put it in the buffer
+        // This is the hard part
+        // For now, just use a fixed pattern
+        index++; // point to data after the ASCII character
+        do {
+          morseChar = pgm_read_byte_near( morseTable + index);
+          if ( morseChar == '#') { // marker for new character ?
+            break;
+          } else {
+            if ( morseChar == '.')
+              strcat( buffer, "X ");
+            else if ( morseChar == '-')
+              strcat( buffer, "XXX ");
+            else if ( morseChar == ' ')
+              strcat( buffer, " ");
+          }
+          index++;
+        } while (morseChar != '\0');
+        interchar_sp();
+      } else {
+        Serial.print("Character not found in morse table: ");
+        if (isprint( serialChar)) {
+          Serial.print( (char) serialChar);
+        } else {
+          Serial.print( "0x");
+          if ( serialChar < 0x10)
+            Serial.print( "0");
+          Serial.print( serialChar, HEX);
+        }
+        Serial.println();
+      }
+    }
+  }
 }
 // END.
