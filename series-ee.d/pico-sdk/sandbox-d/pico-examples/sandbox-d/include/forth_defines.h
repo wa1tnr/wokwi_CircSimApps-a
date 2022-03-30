0d75f3d256d005b853eeea41273508add52b059b
wa1tnr <wa1tnr@users.noreply.github.com>
2022-03-30 21:12:24 +0000

    sync to wokwi dd

--- series-ee.d/pico-sdk/sandbox-d/pico-examples/sandbox-d/include/forth_defines.h
+++ series-ee.d/pico-sdk/sandbox-d/pico-examples/sandbox-d/include/forth_defines.h
@@ -1,7 +1,9 @@
-
+// Wed 30 Mar 20:56:36 UTC 2022
 // #warning seen forth_defines.h
 
-#define RECENT_STAMP      "Sat 25 Dec 20:46:49 UTC 2021"
+// almost all of these are wrong 30 March 2022:
+
+#define RECENT_STAMP      "Wed 30 Mar 20:56:36 UTC 2022"
 #define COMMIT_TIME_STAMP "Wed Oct 27 13:23:00 UTC 2021"
 #define BRANCH_STAMP      "dvlp-aa                         0.1.8-pre-alpha"
 #define COMMIT_STAMP      "2c76db1" // seven characters
