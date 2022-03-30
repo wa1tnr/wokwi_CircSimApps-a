ad07c084e8fc256355ec2b66e8da97a37bb89652
wa1tnr <wa1tnr@users.noreply.github.com>
2022-03-30 15:32:26 +0000

    quick wokwi captures - hh

--- series-ee.d/pico-sdk/sandbox-d/program.h
+++ series-ee.d/pico-sdk/sandbox-d/program.h
@@ -1,1 +1,8 @@
-// Wed 30 Mar 10:34:46 UTC 2022
+// Wed 30 Mar 10:21:14 UTC 2022 - ONLINE edit
+// program.h
+
+// #include "rp2040.h"
+// warn 000: #warning seen program.h
+// #define crlf(__a__); {Serial.print("\r\n");}
+
+extern void reflash_firmware(void); // prototype
