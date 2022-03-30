edfb453e1b17eb7dd13717b023422963fad46f8f
wa1tnr <wa1tnr@users.noreply.github.com>
2022-03-30 10:47:41 +0000

    quick wokwi captures - bb

--- series-ee.d/pico-sdk/sandbox-d/compatibility.h
+++ series-ee.d/pico-sdk/sandbox-d/compatibility.h
@@ -1,1 +1,14 @@
-// Wed 30 Mar 10:35:22 UTC 2022
+// Wed 30 Mar 10:21:14 UTC 2022
+
+// warn 002: #warning seen compatibility.h
+
+#undef  RP2040_VARIANT
+#define RP2040_VARIANT
+
+#ifdef RP2040_VARIANT
+#define RP2040_PRESENT -1 // TRUE
+#endif
+
+#ifndef RP2040_VARIANT
+#define RP2040_PRESENT -0 // FALSE
+#endif
