ec91db8effeec8fa7cc934ea5e9590097523a918
wa1tnr <wa1tnr@users.noreply.github.com>
2022-03-31 01:08:18 +0000

    last capture - dd

--- series-ee.d/pico-sdk/sandbox-d/pico-examples/sandbox-d/include/forth_defines.h
+++ series-ee.d/pico-sdk/sandbox-d/pico-examples/sandbox-d/include/forth_defines.h
@@ -1,9 +1,9 @@
-// Wed 30 Mar 20:56:36 UTC 2022
+// Thu 31 Mar 00:29:05 UTC 2022
 // #warning seen forth_defines.h
 
 // almost all of these are wrong 30 March 2022:
 
-#define RECENT_STAMP      "Wed 30 Mar 20:56:36 UTC 2022"
+#define RECENT_STAMP      "Thu 31 Mar 00:29:05 UTC 2022"
 #define COMMIT_TIME_STAMP "Wed Oct 27 13:23:00 UTC 2021"
 #define BRANCH_STAMP      "dvlp-aa                         0.1.8-pre-alpha"
 #define COMMIT_STAMP      "2c76db1" // seven characters
