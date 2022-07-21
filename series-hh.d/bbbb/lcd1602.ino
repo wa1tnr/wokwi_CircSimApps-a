1a31fee44a35ee7c9ae57956988ea5da0fb4c669
wa1tnr <wa1tnr@users.noreply.github.com>
2022-07-21 07:02:50 +0000

    working simple model

--- series-hh.d/bbbb/lcd1602.ino
+++ series-hh.d/bbbb/lcd1602.ino
@@ -14,20 +14,39 @@ void setup() {
 }
 
 uint32_t raw_ms = millis();
-uint32_t curr_ms = START_MS;
 uint32_t prev_ms = START_MS;
+
+#define DEBUG_AL
+#undef DEBUG_AL
+
+uint16_t clock = 0;
 
 void loop() {
 
   raw_ms = millis();
 
   uint32_t result = raw_ms - prev_ms;
-  Serial.print(result); Serial.print(" ");
 
-  if (result > 800) {
-    curr_ms = raw_ms;
-    prev_ms = curr_ms;
-    Serial.write('z'); Serial.write(' ');
+#ifdef DEBUG_AL
+  Serial.print(result); Serial.print(" ");
+#endif
+
+  if (result > 249) { // print seconds every n milliseconds
+
+    clock = clock + result;
+
+// modulo 60 seconds:
+    if (clock > 59999) { clock = 0; }
+
+    uint8_t scaled_clock = clock / 1000;
+    prev_ms = raw_ms;
+
+    if (scaled_clock < 10) { Serial.print("0");}
+    if (scaled_clock < 100)  { Serial.print("0");}
+
+    Serial.print(scaled_clock);
+    // Serial.write('z');
+    Serial.write(' ');
   }
 }
 // 06:37:54z
