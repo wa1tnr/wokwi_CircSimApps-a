7c79d8e4b3ff4692f41c8f23bd4227ad76f303fc
wa1tnr <wa1tnr@users.noreply.github.com>
2022-07-22 22:00:42 +0000

    copy to revision -bb with dupe hists
    	renamed:    ../t/NOTE.md -> NOTE.md
    	renamed:    ../t/clock_base_60.md -> clock_base_60.md
    	new file:   clock_ticks_LCD-bb.ino
    	renamed:    ../t/diagram.json -> diagram.json
    	renamed:    ../t/n_sketch_clock_lcd.cpp -> n_sketch_clock_lcd.cpp
    	renamed:    ../t/timer_mega_2560.cpp -> timer_mega_2560.cpp
    	deleted:    ../t/clock_ticks_LCD-aa.ino
    
    On branch dvlp-aa

---
+++ series-hh.d/clock_ticks_LCD-bb/clock_ticks_LCD-bb.ino
@@ -0,0 +1,5 @@
+// Fri 22 Jul 22:00:07 UTC 2022 - offline edit for duplication to -bb
+
+// empty sketch - see n_sketch*.cpp
+
+// END.
--- series-hh.d/t/clock_ticks_LCD-aa.ino
+++
@@ -1,5 +0,0 @@
-// Fri 22 Jul 20:24:21 UTC 2022 - offline edit with space aliens sound running
-
-// empty sketch - see n_sketch*.cpp
-
-// END.
