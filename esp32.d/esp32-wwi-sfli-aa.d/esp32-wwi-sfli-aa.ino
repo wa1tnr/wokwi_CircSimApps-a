8723a30d88314ded94239ddbcd41f7182af8e0e7
wa1tnr <wa1tnr@users.noreply.github.com>
2023-06-20 03:08:29 +0000

    online capture

--- esp32.d/esp32-wwi-sfli-aa.d/esp32-wwi-sfli-aa.ino
+++ esp32.d/esp32-wwi-sfli-aa.d/esp32-wwi-sfli-aa.ino
@@ -1,5 +1,10 @@
-// Tue 20 Jun 01:00:33 UTC 2023
+// empty sketch - see main.cpp
 
-// empty sketch
+// os format date/timestamp:
+// Tue 20 Jun 03:02:25 UTC 2023
 
-// END.
+// https://github.com/wa1tnr/wokwi_CircSimApps-a/tree/dvlp-aa/esp32.d/esp32-wwi-sfli-aa.d
+
+// https://wokwi.com/projects/368020117772308481
+
+// end.
