7bd4e1183f61f52703b36294c82cdaad83960c15
wa1tnr <wa1tnr@users.noreply.github.com>
2023-06-21 06:04:05 +0000

    nucleo_g031k8 empty sketch
    see main.cpp

--- stm32.d/nucleo_g031k8.d/rev-aa.d/nucleo_g031k8-fli-wwkwi.ino
+++ stm32.d/nucleo_g031k8.d/rev-aa.d/nucleo_g031k8-fli-wwkwi.ino
@@ -1,4 +1,5 @@
-// Wed 21 Jun 05:18:07 UTC 2023
 // empty sketch
 
-// END.
+//    rev: https://wokwi.com/projects/368121917588349953 06:01z Wed 21st
+
+// wa1tnr  https://wokwi.com/projects/368121790980127745  Wed 21 Jun 05:59:35 UTC 2023
