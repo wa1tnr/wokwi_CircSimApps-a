552a1c4b9ca62cd4b6f1167f7b37c6778425f330
wa1tnr <wa1tnr@users.noreply.github.com>
2023-06-22 22:03:25 +0000

    latest sync copy nucleo forth-like interpreter

--- stm32.d/nucleo_g031k8.d/rev-bb.d/nucleo_g031k8-fli-wwkwi.ino
+++ stm32.d/nucleo_g031k8.d/rev-bb.d/nucleo_g031k8-fli-wwkwi.ino
@@ -2,14 +2,17 @@
 
 // late project uri contains all the one below does:
 
-// https://wokwi.com/projects/368269784655915009
+// https://wokwi.com/projects/368272747143238657
 
 
 
 
 // normal project uri (earlier edit, no real changes):
 
-// wa1tnr https://wokwi.com/projects/368269677501930497  Thu 22 Jun 21:10:06 UTC 2023
+// wa1tnr https://wokwi.com/projects/368272552521237505  Thu 22 Jun 21:55:43 UTC 2023
+
+
+// https://github.com/wa1tnr/wokwi_CircSimApps-a/tree/dvlp-aa/stm32.d/nucleo_g031k8.d/rev-bb.d
 
 // +strobes 21z Thu
 
