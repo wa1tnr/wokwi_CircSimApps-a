93f33e59963be6e98df67cc1a368f824ea3aa546
wa1tnr <wa1tnr@users.noreply.github.com>
2023-06-22 21:49:39 +0000

    new base INO file revision

--- stm32.d/nucleo_g031k8.d/rev-bb.d/nucleo_g031k8-fli-wwkwi.ino
+++ stm32.d/nucleo_g031k8.d/rev-bb.d/nucleo_g031k8-fli-wwkwi.ino
@@ -1,5 +1,20 @@
 // empty sketch
 
-//    rev: https://wokwi.com/projects/368121917588349953 06:01z Wed 21st
+// late project uri contains all the one below does:
 
-// wa1tnr  https://wokwi.com/projects/368121790980127745  Wed 21 Jun 05:59:35 UTC 2023
+// https://wokwi.com/projects/368269784655915009
+
+
+
+
+// normal project uri (earlier edit, no real changes):
+
+// wa1tnr https://wokwi.com/projects/368269677501930497  Thu 22 Jun 21:10:06 UTC 2023
+
+// +strobes 21z Thu
+
+// top to bottom:
+
+//  16, 17, 18, 19, 20 ..
+
+// END
