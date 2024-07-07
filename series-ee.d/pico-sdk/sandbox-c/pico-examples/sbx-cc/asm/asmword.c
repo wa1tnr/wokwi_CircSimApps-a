49cf4a4b9d97547e2ae3b99ea8b42690bc4a1402
wa1tnr <wa1tnr@users.noreply.github.com>
2024-07-07 17:35:45 +0000

    moar new files added
    	new file:   application/application.c
    	new file:   application/application.h
    	new file:   application/rp2040_flashkey.inc
    	new file:   application/rp2040_pico.h
    	new file:   application/rp2040_pico.inc
    	new file:   application/rp2040_pico_getkey_usb.inc
    	new file:   asm/asmword.S
    	new file:   asm/asmword.c
    	new file:   asm/config.S
    	new file:   include/feather.h
    	new file:   include/forth_defines.h
    	new file:   include/itsy.h
    	new file:   include/qtpy.h
    	new file:   include/tiny2040.h
    
    On branch add-files-to-tarball

---
+++ series-ee.d/pico-sdk/sandbox-c/pico-examples/sbx-cc/asm/asmword.c
@@ -0,0 +1,61 @@
+// cloop.c
+// Tue  4 Apr 23:33:11 UTC 2023
+
+#ifndef NOT_WOKWI
+#define HAVE_FOUND_WOKWI_ENVIRONMENT
+// #warning this is very much wokwi
+#endif
+
+#ifdef NOT_WOKWI
+#define HAVE_FOUND_NON_WOKWI_ENVIRONMENT
+// #warning this is not wokwi
+#endif
+
+#define PBLINKS 17 // how many blinks?
+#include "hardware/gpio.h"
+
+#ifdef HAVE_FOUND_WOKWI_ENVIRONMENT
+// #warning this is very much wokwi
+#include <Arduino.h>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void experiment_a_asm();
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+#ifdef HAVE_FOUND_WOKWI_ENVIRONMENT
+// #warning this setup function header
+void setup(void) {
+#endif
+
+#ifdef HAVE_FOUND_NON_WOKWI_ENVIRONMENT
+// #warning this is  not wokwi asmword function header
+void asmword(void) {
+#endif
+    static volatile int c; // not volatile static as it was, prior
+    c = (PBLINKS * 2) + 2;
+
+    // unsigned int ram = &c;
+    // DISCUSS and TODO:
+    // 20 Mar 2024: made the cast (see next line) but did not test for functionality
+    unsigned int ram = (unsigned int) &c;
+
+    return experiment_a_asm(c, ram);
+}
+
+#ifdef HAVE_FOUND_WOKWI_ENVIRONMENT
+// #warning this is very much wokwi
+void loop() {
+    while(-1);
+}
+#endif
+
+// END.
---
+++ series-ee.d/pico-sdk/sandbox-c/pico-examples/sbx-cc/include/forth_defines.h
@@ -0,0 +1,10 @@
+// #warning seen 04 Apr 2023 forth_defines.h
+
+// RECENT_STAMP is for after a commit when there is a change
+#define RECENT_STAMP      "Sat 23 Dec 02:42:08 UTC 2023"
+#define COMMIT_TIME_STAMP "Wed Apr  5 22:54:44 UTC 2023"
+#define BRANCH_STAMP      "asm-dvlp-bugfix special build b 0.1.9-pre-alpha"
+#define COMMIT_STAMP      "ec4a584" // seven characters
+#define FEATURE_STAMP     "+asm_params +ASM BOX +github +no_fla "
+#define WOKWI_PRESENT -1
+
